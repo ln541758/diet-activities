d328219b2e226eaf369508d428be6c1fa2dd35b5
003 <apple@003-MacBook-Air.local>
2024-10-13 01:21:05 -0700

    Finish part1

--- Components/Color.js
+++ Components/Color.js
@@ -2,6 +2,7 @@
  * A collection of colors used throughout the application.
  * This file helps to maintain a consistent color palette and makes it easier to update colors when necessary.
  */
+
 const colors = {
     darkPurple: "#3D348B",
     lightPurple: "#CDC3E2",
