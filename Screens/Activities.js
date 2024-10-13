6c928071c96fc59854375d62faebce2e6b1d643c
003 <apple@003-MacBook-Air.local>
2024-10-13 02:22:16 -0700

    Navigator and style

---
+++ Screens/Activities.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Style from "../Components/Style";
+
+export default function Activities() {
+  return (
+    <View style={Style}>
+      <Text>Activities</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
